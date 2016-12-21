dc97bc2daf0efd6d67c01c1a1b2d345f088d1bcb
Mandaris <info@mandarismoore.com>
2016-12-20 17:29:34 -0800

    Clean up configuration for site
    Remove config information that I’m not using and save the plugins that
    I am using in git

--- pelicanconf.py
+++ pelicanconf.py
@@ -19,7 +19,7 @@ DEBUG = 'TRUE'
 STATIC_PATHS = ['images', 'extra', 'code']
 EXTRA_PATH_METADATA = {'extra/CNAME': {'path': 'CNAME'},
                        'extra/favicon.ico': {'path': 'favicon.ico'},
-                       'extra/favicon.svg': {'path': 'favicon.svg'},
+                       'extra/favicon.png': {'path': 'favicon.png'},
                        'extra/apple-touch-icon.png': {'path': 'apple-touch-icon.png'},}
 
 # URLs
@@ -34,24 +34,18 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-# Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
-
-# Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
 
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
-RELATIVE_URLS = True
+#RELATIVE_URLS = True
 
 # Include plugins
 PLUGIN_PATHS = ['/Users/mandaris/Projects/pelican-plugins']
-PLUGINS = ['liquid_tags.tufte', 'pelican-open_graph', 'liquid_tags.img']
+PLUGINS = ['liquid_tags.tufte', 'pelican-open_graph', 'liquid_tags.img', 'plantuml']
+
+# Markdown Plugins
+MD_EXTENSIONS = ['codehilite(css_class=highlight)','extra']
 
 # Other variables need
 SITE_DESCRIPTION = 'The personal website for Mandaris Moore.'
