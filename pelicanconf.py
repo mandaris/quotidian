5b1d1590c8ef2915df61fba1f5e47c5e572e81ba
Mandaris <info@mandarismoore.com>
2017-11-05 17:34:08 -0800

    Moved variables around
    Cleaned up the variables used for readability and logistics.

--- pelicanconf.py
+++ pelicanconf.py
@@ -9,24 +9,25 @@ SITEURL = 'http://quotidianquest.com'
 PATH = 'content'
 
 TIMEZONE = 'US/Pacific'
-
+SITE_DESCRIPTION = 'The personal website for Mandaris Moore.'
 DEFAULT_LANG = u'en'
 LOCALE = u'en_US'
+DEFAULT_PAGINATION = 10
 
 # Site customization
 THEME = u'/Users/mandaris/Projects/TuftePelican'
-DEBUG = 'TRUE'
+DISPLAY_PAGES_ON_MENU = True
 STATIC_PATHS = ['images', 'extra', 'code']
 EXTRA_PATH_METADATA = {'extra/CNAME': {'path': 'CNAME'},
                        'extra/favicon.ico': {'path': 'favicon.ico'},
                        'extra/favicon.png': {'path': 'favicon.png'},
                        'extra/apple-touch-icon.png': {'path': 'apple-touch-icon.png'},}
 
-# URLs
+## URLs
 ARTICLE_URL = '{date:%Y}/{date:%m}/{slug}.html'
 ARTICLE_SAVE_AS = '{date:%Y}/{date:%m}/{slug}.html'
 
-# Feed generation is usually not desired when developing
+## Feed generation (usually not desired when developing)
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 FEED_ATOM = 'feeds/atom.xml'
 CATEGORY_FEED_ATOM = None
@@ -35,11 +36,6 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 
-DEFAULT_PAGINATION = 10
-
-# Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
-
 # Include plugins
 PLUGIN_PATHS = ['/Users/mandaris/Projects/pelican-plugins']
 PLUGINS = ['liquid_tags.tufte', 'pelican-open_graph', 'liquid_tags.img', 'plantuml']
@@ -47,8 +43,10 @@ PLUGINS = ['liquid_tags.tufte', 'pelican-open_graph', 'liquid_tags.img', 'plantu
 # Markdown Plugins
 MD_EXTENSIONS = ['codehilite(css_class=highlight)','extra']
 
-# Other variables need
-SITE_DESCRIPTION = 'The personal website for Mandaris Moore.'
+# Other variables 
 OPEN_GRAPH = True
 LOAD_CONTENT_CACHE = False 
 
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+#DEBUG = True
